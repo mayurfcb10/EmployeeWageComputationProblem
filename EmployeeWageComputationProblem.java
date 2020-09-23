69c018349d34cae446a445b08daa21dece01098b
Mayur <mayurzope555@gmail.com>
2020-09-23 10:45:30 +0530

    [Mayur]Added: Welcome to the Employee Wage Computation Problem

---
+++ EmployeeWageComputationProblem.java
@@ -0,0 +1,7 @@
+/* Welcome to the Employee Wage Computation Problem */
+
+public class EmployeeWageComputationProblem {
+		public static void main(String[] args) {
+		System.out.println("Welcome to the Employee Wage Computation Problem");
+	}
+}
