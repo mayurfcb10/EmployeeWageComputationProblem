3215c4be6c60c7c84e2e880a2b8f450ad8001b60
Mayur <mayurzope555@gmail.com>
2020-09-23 12:36:56 +0530

    [Mayur]Added: Added the Employee Monthly Wage

--- EmployeeWageComputationProblem.java
+++ EmployeeWageComputationProblem.java
@@ -1,26 +1,41 @@
 /* Welcome to the Employee Wage Computation Problem */
 
 public class EmployeeWageComputationProblem {
+
 		public static final int IS_PART_TIME = 1;
 		public static final int IS_FULL_TIME = 2;
 		public static final int EMP_RATE_PER_HOUR = 20;
+		public static final int NUM_OF_WORKING_DAYS = 20;
+	
 		public static void main(String[] args) {
 			System.out.println("Welcome to the Employee Wage Computation Problem");
 			int empHrs = 0;
-			int empWage = 0;	
-			double employeeCheck = (int)Math.floor(Math.random() * 10) % 3;
-			switch ((int)employeeCheck) {
-				case IS_PART_TIME:
-					empHrs = 4;
-					break;	
-				case IS_FULL_TIME:
-					empHrs = 8;
-					break;
-				default:
-					empHrs = 0;
+			int empWage = 0;
+			int totalEmpWage = 0;	
+	
+			for(int day = 0; day < NUM_OF_WORKING_DAYS; day++) {
+		
+				double employeeCheck = (int)Math.floor(Math.random() * 10) % 3;
+
+				switch ((int)employeeCheck) {
+						case IS_PART_TIME:
+						empHrs = 4;
+						break;	
+
+						case IS_FULL_TIME:
+						empHrs = 8;
+						break;
+			
+						default:
+						empHrs = 0;
 			}
 
 			empWage = empHrs * EMP_RATE_PER_HOUR;
 			System.out.println("Emp Wage: " + empWage);
+			
+			}
+
+			System.out.println("Total Emp Wage: "+ totalEmpWage);
+	
 	}
 }
