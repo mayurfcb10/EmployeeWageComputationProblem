8c5dd93970b4de93c9231a7dc452cda755237fd6
Mayur <mayurzope555@gmail.com>
2020-09-23 11:32:41 +0530

    [Mayur]Added: Employee Part Time Wage Computation

--- EmployeeWageComputationProblem.java
+++ EmployeeWageComputationProblem.java
@@ -3,16 +3,21 @@
 public class EmployeeWageComputationProblem {
 		public static void main(String[] args) {
 		System.out.println("Welcome to the Employee Wage Computation Problem");
-			// Constants
-		int IS_EMPLOYEE_FULL_TIME = 1;
-	 	int EMP_RATE_PER_HOUR = 20;
+		int IS_PART_TIME = 1;
+		int IS_FULL_TIME = 2;
+		int EMP_RATE_PER_HOUR = 20;
 		int empHrs = 0;
 		int empWage = 0;	
 			// Computation involved to check the Employee 
-		double employeeCheck = Math.floor(Math.random() * 10) % 2;
+		double employeeCheck = Math.floor(Math.random() * 10) % 3;
 	
-		if(employeeCheck == IS_EMPLOYEE_FULL_TIME){
+		if(employeeCheck == IS_FULL_TIME){
 			empHrs = 8;
+			// Full time work of the employee as 8 hours.
+		}
+		else if (employeeCheck == IS_PART_TIME){
+			empHrs = 4;
+			// Part time hour for the employee as 4 hours.
 		}
 		else{
 			empHrs = 0;
