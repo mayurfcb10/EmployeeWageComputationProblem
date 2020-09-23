194f9b9ee29cf40a16682a54ca5d150466b70e09
Mayur <mayurzope555@gmail.com>
2020-09-23 12:04:24 +0530

    [Mayur]Added: Employee Wage Computation using the Switch Case

--- EmployeeWageComputationProblem.java
+++ EmployeeWageComputationProblem.java
@@ -1,28 +1,26 @@
 /* Welcome to the Employee Wage Computation Problem */
 
 public class EmployeeWageComputationProblem {
+		public static final int IS_PART_TIME = 1;
+		public static final int IS_FULL_TIME = 2;
+		public static final int EMP_RATE_PER_HOUR = 20;
 		public static void main(String[] args) {
-		System.out.println("Welcome to the Employee Wage Computation Problem");
-		int IS_PART_TIME = 1;
-		int IS_FULL_TIME = 2;
-		int EMP_RATE_PER_HOUR = 20;
-		int empHrs = 0;
-		int empWage = 0;	
-			// Computation involved to check the Employee 
-		double employeeCheck = Math.floor(Math.random() * 10) % 3;
-	
-		if(employeeCheck == IS_FULL_TIME){
-			empHrs = 8;
-			// Full time work of the employee as 8 hours.
-		}
-		else if (employeeCheck == IS_PART_TIME){
-			empHrs = 4;
-			// Part time hour for the employee as 4 hours.
-		}
-		else{
-			empHrs = 0;
-		}
-		empWage = empHrs * EMP_RATE_PER_HOUR;
-		System.out.println("Emp Wage: " + empWage);
+			System.out.println("Welcome to the Employee Wage Computation Problem");
+			int empHrs = 0;
+			int empWage = 0;	
+			double employeeCheck = (int)Math.floor(Math.random() * 10) % 3;
+			switch ((int)employeeCheck) {
+				case IS_PART_TIME:
+					empHrs = 4;
+					break;	
+				case IS_FULL_TIME:
+					empHrs = 8;
+					break;
+				default:
+					empHrs = 0;
+			}
+
+			empWage = empHrs * EMP_RATE_PER_HOUR;
+			System.out.println("Emp Wage: " + empWage);
 	}
 }
