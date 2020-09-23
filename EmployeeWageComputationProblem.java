5df23849e8cd6d7981b45862d503f719507f3370
Mayur <mayurzope555@gmail.com>
2020-09-23 14:04:24 +0530

    [Mayur]Added: Employee Wage computation considering total working hours and total number working of days in a month

--- EmployeeWageComputationProblem.java
+++ EmployeeWageComputationProblem.java
@@ -6,36 +6,37 @@ public class EmployeeWageComputationProblem {
 		public static final int IS_FULL_TIME = 2;
 		public static final int EMP_RATE_PER_HOUR = 20;
 		public static final int NUM_OF_WORKING_DAYS = 20;
-	
+		public static final int MAX_HRS_IN_MONTH = 100;	
+		
 		public static void main(String[] args) {
 			System.out.println("Welcome to the Employee Wage Computation Problem");
 			int empHrs = 0;
-			int empWage = 0;
-			int totalEmpWage = 0;	
+			int totalWorkingDays = 0;
+			int totalEmpHrs = 0;	
 	
-			for(int day = 0; day < NUM_OF_WORKING_DAYS; day++) {
-		
-				double employeeCheck = (int)Math.floor(Math.random() * 10) % 3;
+			while(totalEmpHrs <= MAX_HRS_IN_MONTH && totalWorkingDays < NUM_OF_WORKING_DAYS) {
+			totalWorkingDays++;
+			int employeeCheck = (int)Math.floor(Math.random() * 10) % 3;
 
-				switch ((int)employeeCheck) {
-						case IS_PART_TIME:
-						empHrs = 4;
-						break;	
+			switch (employeeCheck) {
+				case IS_PART_TIME:
+					empHrs = 4;
+					break;	
 
-						case IS_FULL_TIME:
-						empHrs = 8;
-						break;
+				case IS_FULL_TIME:
+					empHrs = 8;
+					break;
 			
-						default:
-						empHrs = 0;
+				default:
+					empHrs = 0;
 			}
 
-			empWage = empHrs * EMP_RATE_PER_HOUR;
-			System.out.println("Emp Wage: " + empWage);
-			
+			totalEmpHrs += empHrs;
+			System.out.println("Day#: "+ totalWorkingDays +"Emp Hr: "+empHrs);
+
 			}
 
-			System.out.println("Total Emp Wage: "+ totalEmpWage);
-	
-	}
+			int totalEmpWage = totalEmpHrs * EMP_RATE_PER_HOUR;
+			System.out.println("Total Emp Wage: " + totalEmpWage);	
+		}
 }
