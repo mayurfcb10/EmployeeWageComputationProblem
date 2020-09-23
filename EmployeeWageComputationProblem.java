2cb6c6127c37d306d98ece2f825c0bf1eb1de1c7
Mayur <mayurzope555@gmail.com>
2020-09-23 11:08:59 +0530

    [Mayur]Added: Calculated the daily Employee Wage

--- EmployeeWageComputationProblem.java
+++ EmployeeWageComputationProblem.java
@@ -5,13 +5,19 @@ public class EmployeeWageComputationProblem {
 		System.out.println("Welcome to the Employee Wage Computation Problem");
 			// Constants
 		int IS_EMPLOYEE_FULL_TIME = 1;
+	 	int EMP_RATE_PER_HOUR = 20;
+		int empHrs = 0;
+		int empWage = 0;	
 			// Computation involved to check the Employee 
 		double employeeCheck = Math.floor(Math.random() * 10) % 2;
+	
 		if(employeeCheck == IS_EMPLOYEE_FULL_TIME){
-			System.out.println("Employee is Present");
+			empHrs = 8;
 		}
 		else{
-			System.out.println("Employee is Absent");
+			empHrs = 0;
 		}
+		empWage = empHrs * EMP_RATE_PER_HOUR;
+		System.out.println("Emp Wage: " + empWage);
 	}
 }
