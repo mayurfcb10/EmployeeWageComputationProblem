de474ef2188fd0abd97b2ea9d52084c2a35b1758
Mayur <mayurzope555@gmail.com>
2020-09-26 14:27:58 +0530

    [Mayur]:ADDED: Employee Wage Computation Problem for Multiple Companies

--- EmployeeWageComputationProblem.java
+++ EmployeeWageComputationProblem.java
@@ -4,15 +4,15 @@ public class EmployeeWageComputationProblem {
 
 	public static final int IS_PART_TIME = 1;
 	public static final int IS_FULL_TIME = 2;
-	public static final int EMP_RATE_PER_HOUR = 20;
-	public static final int NUM_OF_WORKING_DAYS = 2;
-	public static final int MAX_HRS_IN_MONTH = 10;
+	//public static final int EMP_RATE_PER_HOUR = 20;
+	//public static final int NUM_OF_WORKING_DAYS = 20;
+	//public static final int MAX_HRS_IN_MONTH = 100;
 
-	public static void computeEmployeeWage() {
+	public static void computeEmployeeWage(String company, int empRate, int numOfWorkingDays, int maxHrs) {
 		// variables
 		int empHrs = 0, totalWorkingDays = 0, totalEmpHrs = 0;	
 		// computation
-		while(totalEmpHrs <= MAX_HRS_IN_MONTH && totalWorkingDays < NUM_OF_WORKING_DAYS) {
+		while(totalEmpHrs <= maxHrs && totalWorkingDays < numOfWorkingDays) {
 			totalWorkingDays++;
 			int employeeCheck = (int)Math.floor(Math.random() * 10) % 3;
 			// Check whether Employee is working as a full time or part time or not working.
@@ -29,13 +29,16 @@ public class EmployeeWageComputationProblem {
 			totalEmpHrs += empHrs;
 			System.out.println("Day#: "+ totalWorkingDays +"Emp Hr: "+empHrs);
 		}
-		int totalEmpWage = totalEmpHrs * EMP_RATE_PER_HOUR;
+		int totalEmpWage = totalEmpHrs * empRate;
 		// Total Employee Wage for the hours Employee has worked
-		System.out.println("Total Emp Wage: " + totalEmpWage);
+		System.out.println("Total Emp Wage for Company "+company+" is: "+ totalEmpWage);
 	}
 
 	public static void main(String[] args){
-		System.out.println(" Welcome to the Employee Wage Computation Problem ");
-		computeEmployeeWage();
+		System.out.println("Welcome to the Employee Wage Computation Problem");
+		// Compute the daily wage of the Employee of the respective company 
+		computeEmployeeWage("DMart", 20, 2, 10);
+		System.out.println();
+		computeEmployeeWage("JioMart", 10, 4, 40);
 	}
 }
