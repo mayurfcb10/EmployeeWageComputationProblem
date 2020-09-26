bc8b24549999d2803316620636b4c2cd44e2c492
Mayur <mayurzope555@gmail.com>
2020-09-26 13:59:01 +0530

    [Mayur]:Added: Employee Wage Computation Problem with Class Method

--- EmployeeWageComputationProblem.java
+++ EmployeeWageComputationProblem.java
@@ -2,41 +2,40 @@
 
 public class EmployeeWageComputationProblem {
 
-		public static final int IS_PART_TIME = 1;
-		public static final int IS_FULL_TIME = 2;
-		public static final int EMP_RATE_PER_HOUR = 20;
-		public static final int NUM_OF_WORKING_DAYS = 20;
-		public static final int MAX_HRS_IN_MONTH = 100;	
-		
-		public static void main(String[] args) {
-			System.out.println("Welcome to the Employee Wage Computation Problem");
-			int empHrs = 0;
-			int totalWorkingDays = 0;
-			int totalEmpHrs = 0;	
-	
-			while(totalEmpHrs <= MAX_HRS_IN_MONTH && totalWorkingDays < NUM_OF_WORKING_DAYS) {
+	public static final int IS_PART_TIME = 1;
+	public static final int IS_FULL_TIME = 2;
+	public static final int EMP_RATE_PER_HOUR = 20;
+	public static final int NUM_OF_WORKING_DAYS = 2;
+	public static final int MAX_HRS_IN_MONTH = 10;
+
+	public static void computeEmployeeWage() {
+		// variables
+		int empHrs = 0, totalWorkingDays = 0, totalEmpHrs = 0;	
+		// computation
+		while(totalEmpHrs <= MAX_HRS_IN_MONTH && totalWorkingDays < NUM_OF_WORKING_DAYS) {
 			totalWorkingDays++;
 			int employeeCheck = (int)Math.floor(Math.random() * 10) % 3;
-
+			// Check whether Employee is working as a full time or part time or not working.
 			switch (employeeCheck) {
-				case IS_PART_TIME:
-					empHrs = 4;
-					break;	
-
-				case IS_FULL_TIME:
-					empHrs = 8;
-					break;
-			
-				default:
-					empHrs = 0;
+			case IS_PART_TIME:
+				empHrs = 4;
+				break;	
+			case IS_FULL_TIME:
+				empHrs = 8;
+				break;
+			default:
+				empHrs = 0;
 			}
-
 			totalEmpHrs += empHrs;
 			System.out.println("Day#: "+ totalWorkingDays +"Emp Hr: "+empHrs);
-
-			}
-
-			int totalEmpWage = totalEmpHrs * EMP_RATE_PER_HOUR;
-			System.out.println("Total Emp Wage: " + totalEmpWage);	
 		}
+		int totalEmpWage = totalEmpHrs * EMP_RATE_PER_HOUR;
+		// Total Employee Wage for the hours Employee has worked
+		System.out.println("Total Emp Wage: " + totalEmpWage);
+	}
+
+	public static void main(String[] args){
+		System.out.println(" Welcome to the Employee Wage Computation Problem ");
+		computeEmployeeWage();
+	}
 }
